d2b38d5c605addbd14cd3bcbb9bfe3fe6f387340
Oguzcan Ayan <oguzcanayan43@hotmail.com>
2023-07-28 17:26:47 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "primary-100": "#28338B",
+        "primary-300": "#C74BD2",
+        "primary-500": "#1F1F1F",
+        "gradient-100": "#3C1DFF",
+        "gradient-300": "#14C9C9"
+      },
+  
+    content: {
+      test100: "url('./img/a1.png')",
+      test300: "url('./img/a2.png')",
+      }
+    },
+  },
+  plugins: [],
+}
+
